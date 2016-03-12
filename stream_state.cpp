e9a64fae0e2e399ce6e273d9fd813b906893a6af
charles-pku-2013 <chao.sun.2010@gmail.com>
2016-03-13 04:40:24 +0800

    changed

--- stream_state.cpp
+++ stream_state.cpp
@@ -18,9 +18,9 @@ using namespace std;
   当用ifs.read读完文件时，good = 0, eof = 1, fail = 1, bad = 0
   此时若想判断 fstream 状态，应该用 if(!ifs.bad()) 或
   if( !ifs ) {
-	  if( ifs.eof() )
-		  // 当success处理
-	  else // fail
+      if( ifs.eof() )
+          // 当success处理
+      else // fail
   }
 */
 
@@ -35,26 +35,28 @@ void print_stream_state( const StreamType &stream )
     cout << bitset<4>(stream.rdstate()).to_string() << endl;
 }
 
+
+void test1()
+{
+    vector<int> vec;
+    copy( istream_iterator<int>(cin), istream_iterator<int>(), back_inserter(vec) );
+    print_stream_state( cin );
+}
+
+void test2()
+{
+    int n, m;
+    string s("10a, 21");
+    stringstream str(s);
+    str >> n >> m;
+    cout << n << endl;
+    print_stream_state( str );
+}
+
 int main()
 {
-    // test 1
-    /*
-     * {
-     *     vector<int> vec;
-     *     copy( istream_iterator<int>(cin), istream_iterator<int>(), back_inserter(vec) );
-     *     print_stream_state( cin );
-     * }
-     */
-
-    // test 2
-    {
-        int n, m;
-        string s("10a, 21");
-        stringstream str(s);
-        str >> n >> m;
-        cout << n << endl;
-        print_stream_state( str );
-    }
+    test1();
+    // test2();
 
     return 0;
 }
