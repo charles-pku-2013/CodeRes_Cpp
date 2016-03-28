09172b4dcf4bae3cd25f6dfea5e6643e47931145
charles-pku-2013 <chao.sun.2010@gmail.com>
2016-03-28 21:40:49 +0800

    Add bad_weak_ptr demo

--- Cpp11/weak_ptr_multi_thread.cpp
+++ Cpp11/weak_ptr_multi_thread.cpp
@@ -14,17 +14,26 @@ struct Foo {
 };
  
 static
+void func_called_by_thread( const std::shared_ptr<Foo> &sp )
+{
+    if( sp )
+        cout << "sp count is: " << sp.use_count() << endl;
+}
+
+static
 void thread_routine( std::weak_ptr<Foo> wp )
 {
     for( ; ; ) {
         std::this_thread::sleep_for(std::chrono::seconds(1));
-        if( auto sp = wp.lock() ) {
-            cout << "sp from weak_ptr use_count is: " << sp.use_count() << endl;
-            sp->greet();
-        } else {
-            cout << "Object has been destroyed!" << endl;
-            return;
-        } // if
+        //!! shared_ptr 可以隐式转换成weak_ptr，但反过来不行
+        func_called_by_thread( std::shared_ptr< Foo >(wp) ); // throw bad_weak_ptr if fail.
+        // if( auto sp = wp.lock() ) {
+            // cout << "sp from weak_ptr use_count is: " << sp.use_count() << endl;
+            // sp->greet();
+        // } else {
+            // cout << "Object has been destroyed!" << endl;
+            // return;
+        // } // if
     } // for
 
     return;
@@ -32,26 +41,32 @@ void thread_routine( std::weak_ptr<Foo> wp )
  
 int main()
 {
-    std::shared_ptr<Foo> pFoo = std::make_shared<Foo>();
-    cout << "After make_shared, pFoo use_count is: " << pFoo.use_count() << endl;
-    
-    /*
-     * 下面这行用法是错误的。
-     * 要知道thread对象如同 function object 一样，
-     * 存储了传入参数的拷贝pFoo, 这样使之增加了一个引用。
-     * 同样的问题也存在于bind，用的时候要格外注意!!!
-     */
-    // std::thread t1( thread_routine, pFoo );
-    //!! This is the right way to use.
-    std::thread t1( thread_routine, std::weak_ptr<Foo>(pFoo) );
-    cout << "After creating thread, pFoo use_count is: " << pFoo.use_count() << endl;
+    try {
+        std::shared_ptr<Foo> pFoo = std::make_shared<Foo>();
+        cout << "After make_shared, pFoo use_count is: " << pFoo.use_count() << endl;
 
-    std::this_thread::sleep_for(std::chrono::seconds(5));
-    cout << "Before reset pFoo use count is: " << pFoo.use_count() << endl;
-    pFoo.reset();
+        /*
+         * 下面这行用法是错误的。
+         * 要知道thread对象如同 function object 一样，
+         * 存储了传入参数的拷贝pFoo, 这样使之增加了一个引用。
+         * 同样的问题也存在于bind，用的时候要格外注意!!!
+         */
+        // std::thread t1( thread_routine, pFoo );
+        //!! This is the right way to use.
+        std::thread t1( thread_routine, std::weak_ptr<Foo>(pFoo) );
+        cout << "After creating thread, pFoo use_count is: " << pFoo.use_count() << endl;
 
-    if( t1.joinable() )
-        t1.join();
+        std::this_thread::sleep_for(std::chrono::seconds(5));
+        cout << "Before reset pFoo use count is: " << pFoo.use_count() << endl;
+        pFoo.reset();
+
+        if( t1.joinable() )
+            t1.join();
+
+    } catch ( const std::exception &ex ) {
+        cerr << ex.what() << endl;
+        exit(-1);
+    }
     
     return 0;
 }
