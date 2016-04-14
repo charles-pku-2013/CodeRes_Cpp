1a788f8fcd321b0b943cfe933d280c6de2012db3
charles-pku-2013 <chao.sun.2010@gmail.com>
2016-04-14 11:48:13 +0800

    revised

--- Cpp11/shared_ptr_pitfall_circular_ref_循环引用.cpp
+++ Cpp11/shared_ptr_pitfall_circular_ref_循环引用.cpp
@@ -15,14 +15,18 @@ struct A {
     ~A()
     { cout << "A destructor." << endl; }
 
+    long val;
     shared_ptr<B>   m_b;
+    // weak_ptr<B>   m_b;
 };
  
 struct B {
     ~B()
     { cout << "B destructor." << endl; }
 
+    long val;
     shared_ptr<A>   m_a;
+    // weak_ptr<A>   m_a;
 };
  
 int main()
@@ -47,3 +51,35 @@ int main()
 
     return 0; 
 }
+
+
+// valgrind output for circular depend.
+/*
+ * ==4897== HEAP SUMMARY:
+ * ==4897==     in use at exit: 96 bytes in 4 blocks
+ * ==4897==   total heap usage: 4 allocs, 0 frees, 96 bytes allocated
+ * ==4897== 
+ * ==4897== 96 (24 direct, 72 indirect) bytes in 1 blocks are definitely lost in loss record 4 of 4
+ * ==4897==    at 0x4C2B0E0: operator new(unsigned long) (in /usr/lib/valgrind/vgpreload_memcheck-amd64-linux.so)
+ * ==4897==    by 0x400D42: main (test.cpp:32)
+ * ==4897== 
+ * ==4897== LEAK SUMMARY:
+ * ==4897==    definitely lost: 24 bytes in 1 blocks
+ * ==4897==    indirectly lost: 72 bytes in 3 blocks
+ * ==4897==      possibly lost: 0 bytes in 0 blocks
+ * ==4897==    still reachable: 0 bytes in 0 blocks
+ * ==4897==         suppressed: 0 bytes in 0 blocks
+ */
+
+// valgrind output when switch to weak_ptr
+/*
+ * ==4922== HEAP SUMMARY:
+ * ==4922==     in use at exit: 0 bytes in 0 blocks
+ * ==4922==   total heap usage: 4 allocs, 4 frees, 96 bytes allocated
+ * ==4922== 
+ * ==4922== All heap blocks were freed -- no leaks are possible
+ * ==4922== 
+ * ==4922== For counts of detected and suppressed errors, rerun with: -v
+ * ==4922== ERROR SUMMARY: 0 errors from 0 contexts (suppressed: 0 from 0)
+ */
+
