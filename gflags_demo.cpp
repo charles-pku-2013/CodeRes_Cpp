34e274175888907ba3aefe1fefeef3764f26c1d2
charles-pku-2013 <chao.sun.2010@gmail.com>
2016-05-18 18:16:50 +0800

    revised

--- gflags_demo.cpp
+++ gflags_demo.cpp
@@ -4,6 +4,29 @@
  * /tmp/test -nobig_menu --languages="Chinese,English,Japanese"
  */
 
+/*
+ * Usage:
+ * app_containing_foo --nobig_menu -languages="chinese,japanese,korean" ...
+ * app_containing_foo --languages="chinese,japanese,korean"
+ * app_containing_foo -languages="chinese,japanese,korean"
+ * app_containing_foo --languages "chinese,japanese,korean"
+ * app_containing_foo -languages "chinese,japanese,korean"
+ * app_containing_foo --big_menu
+ * app_containing_foo --nobig_menu
+ * app_containing_foo --big_menu=true
+ * app_containing_foo --big_menu=false
+ */
+
+/*
+ * types:
+ * DEFINE_bool: boolean
+ * DEFINE_int32: 32-bit integer
+ * DEFINE_int64: 64-bit integer
+ * DEFINE_uint64: unsigned 64-bit integer
+ * DEFINE_double: double
+ * DEFINE_string: C++ string
+ */
+
 #include <iostream>
 #include <cstdio>
 #include <gflags/gflags.h>
@@ -25,7 +48,7 @@ DEFINE_int32(port, 8888, "What port to listen on");
 static 
 bool ValidatePort(const char* flagname, gflags::int32 value) 
 {
-    // cout << "ArgValidator() flagname = " << flagname << " value = " << value << endl;
+    cout << "ArgValidator() flagname = " << flagname << " value = " << value << endl;
     if (value > 1024 && value < 32768)   // value is ok
         return true;
     printf("Invalid value for --%s: %d\n", flagname, (int)value);
