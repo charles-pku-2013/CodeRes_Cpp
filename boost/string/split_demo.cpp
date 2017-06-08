98157a281aedec9cb7ced4ba5160bb02aa966f5b
charles-pku-2013 <chao.sun.2010@gmail.com>
2017-06-08 14:14:45 +0800

    saved

--- boost/string/split_demo.cpp
+++ boost/string/split_demo.cpp
@@ -4,6 +4,7 @@
 #include <algorithm>
 #include <boost/algorithm/string.hpp>
 
+#define SPACES                " \t\f\r\v\n"
 
 int main()
 {
@@ -12,6 +13,8 @@ int main()
     // NOTE!!! 分隔符出现在开头或末尾会导致最后空字符串的产生
     // 所以一般在split之前先用分隔符trim
     string line = "123,4,,5,6,"; 
+    // boost::trim_if(line, boost::is_any_of(string(",") + SPACES));
+
     vector<string> values;
     boost::split(values, line, boost::is_any_of(","), boost::token_compress_on);
     
