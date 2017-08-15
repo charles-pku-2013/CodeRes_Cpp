43e232db0dc9bd1e8cb86fab17ccc69ad2ac99e2
charles-pku-2013 <chao.sun.2010@gmail.com>
2017-08-15 18:59:19 +0800

    boost program_options explain

--- boost/program_options/first.cpp
+++ boost/program_options/first.cpp
@@ -1,3 +1,6 @@
+/*
+ * c++ -o /tmp/test first.cpp -lboost_program_options -std=c++11 -g
+ */
 // Copyright Vladimir Prus 2002-2004.
 // Distributed under the Boost Software License, Version 1.0.
 // (See accompanying file LICENSE_1_0.txt
@@ -13,15 +16,24 @@ namespace po = boost::program_options;
 #include <iterator>
 using namespace std;
 
+/*
+ * default_value() is the value that will be put in the variables_map if the user didn't specify another value:
+ * implicit_value() is the value that will be used if the user specifies the option but without an adjacent value.
+ * required 必须提供的参数
+ */
+
 int main(int ac, char* av[])
 {
     try {
 
+        int count = 0;
+
         po::options_description desc("Allowed options");
         desc.add_options()
             ("help,h", "produce help message")
-            ("compression,c", po::value<double>(), "set compression level")
-            ("debug", po::value<bool>(), "debug mode");
+            ("compression,c", po::value<double>()->default_value(9), "set compression level")
+            ("debug", po::value<bool>()->default_value(false), "debug mode")
+            ("count,n", po::value<int>(&count)->required(), "set count");   // 设置已有变量
 
         po::variables_map vm;
         po::store(po::parse_command_line(ac, av, desc), vm);
@@ -40,11 +52,22 @@ int main(int ac, char* av[])
         }
 
         // cout << vm["test"].as<int>() << endl;
-        auto it = vm.find("compression");
-        if (it == vm.end()) cout << "compression not set!" << endl;
-        else cout << "compression set to " << it->second.as<double>() << endl;
 
-        cout << vm["debug"].as<bool>() << endl;
+        // check compression
+        {
+            auto it = vm.find("compression");
+            if (it == vm.end()) cout << "compression not set!" << endl;
+            else cout << "compression set to " << it->second.as<double>() << endl;
+        }
+
+        // check debug
+        {
+            auto it = vm.find("debug");
+            if (it != vm.end())
+                cout << "debug mode: " << it->second.as<bool>() << endl;
+        }
+
+        cout << "count = " << count << endl;
 
     }
     catch(exception& e) {
--- boost/program_options/multiple_sources.cpp
+++ boost/program_options/multiple_sources.cpp
@@ -1,9 +1,14 @@
+/*
+ * c++ -o /tmp/test multiple_sources.cpp -lboost_program_options -std=c++11 -g
+ */
 // Copyright Vladimir Prus 2002-2004.
 // Distributed under the Boost Software License, Version 1.0.
 // (See accompanying file LICENSE_1_0.txt
 // or copy at http://www.boost.org/LICENSE_1_0.txt)
 
 /* Shows how to use both command line and config file. */
+
+// 命令行参数会覆盖配置文件
 
 #include <boost/program_options.hpp>
 namespace po = boost::program_options;
@@ -18,18 +23,22 @@ using namespace std;
 template<class T>
 ostream& operator<<(ostream& os, const vector<T>& v)
 {
-    copy(v.begin(), v.end(), ostream_iterator<T>(os, " ")); 
+    copy(v.begin(), v.end(), ostream_iterator<T>(os, " "));
     return os;
 }
 
-
+/*
+ * composing: values from different sources should be composed.
+ * Otherwise, value from the first source is used and values from
+ * other sources are discarded.
+ */
 int main(int ac, char* av[])
 {
     try {
         int opt;
         string config_file;
-    
-        // Declare a group of options that will be 
+
+        // Declare a group of options that will be
         // allowed only on command line
         po::options_description generic("Generic options");
         generic.add_options()
@@ -38,16 +47,17 @@ int main(int ac, char* av[])
             ("config,c", po::value<string>(&config_file)->default_value("multiple_sources.cfg"),
                   "name of a file of a configuration.")
             ;
-    
-        // Declare a group of options that will be 
+
+        // Declare a group of options that will be
         // allowed both on command line and in
         // config file
         po::options_description config("Configuration");
         config.add_options()
-            ("optimization", po::value<int>(&opt)->default_value(10), 
+            ("optimization", po::value<int>(&opt)->default_value(10),
                   "optimization level")
-            ("include-path,I", 
-                 po::value< vector<string> >()->composing(), 
+            ("include-path,I",
+                 // po::value< vector<string> >()->composing(),
+                 po::value< vector<string> >()->multitoken(), // 💜💜 参数列表指定方式：/tmp/test -I/usr -I/var -I/opt
                  "include path")
             ;
 
@@ -58,7 +68,7 @@ int main(int ac, char* av[])
             ("input-file", po::value< vector<string> >(), "input file")
             ;
 
-        
+
         po::options_description cmdline_options;
         cmdline_options.add(generic).add(config).add(hidden);
 
@@ -67,15 +77,15 @@ int main(int ac, char* av[])
 
         po::options_description visible("Allowed options");
         visible.add(generic).add(config);
-        
+
         po::positional_options_description p;
         p.add("input-file", -1);
-        
+
         po::variables_map vm;
         store(po::command_line_parser(ac, av).
               options(cmdline_options).positional(p).run(), vm);
         notify(vm);
-        
+
         ifstream ifs(config_file.c_str());
         if (!ifs)
         {
@@ -84,10 +94,10 @@ int main(int ac, char* av[])
         }
         else
         {
-            store(parse_config_file(ifs, config_file_options), vm);
-            notify(vm);
+            po::store(po::parse_config_file(ifs, config_file_options), vm);
+            po::notify(vm);
         }
-    
+
         if (vm.count("help")) {
             cout << visible << "\n";
             return 0;
@@ -100,22 +110,26 @@ int main(int ac, char* av[])
 
         if (vm.count("include-path"))
         {
-            cout << "Include paths are: " 
+            cout << "Include paths are: "
                  << vm["include-path"].as< vector<string> >() << "\n";
+            const vector<string> &paths = vm["include-path"].as< vector<string> >();
+            cout << "include-path size = " << paths.size() << endl;
         }
 
         if (vm.count("input-file"))
         {
-            cout << "Input files are: " 
+            cout << "Input files are: "
                  << vm["input-file"].as< vector<string> >() << "\n";
         }
 
-        cout << "Optimization level is " << opt << "\n";                
+        cout << "Optimization level is " << opt << "\n";
     }
     catch(exception& e)
     {
         cout << e.what() << "\n";
         return 1;
-    }    
+    }
+
+
     return 0;
 }
