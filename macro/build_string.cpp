3cc87ab43a0e93e9e0c7913e678a182787cf20e8
charles-pku-2013 <chao.sun.2010@gmail.com>
2017-08-20 02:02:22 +0800

    saved

--- macro/build_string.cpp
+++ macro/build_string.cpp
@@ -2,7 +2,7 @@
 #include <sstream>
 #include <string>
 
-
+// 🔴🔵cast类型转换优先级没有函数调用.->运算符高，遇到都要加括号
 #define BUILD_STRING(args)  \
         (static_cast<std::ostringstream&>(std::ostringstream()  \
                   << args << std::flush)).str()
