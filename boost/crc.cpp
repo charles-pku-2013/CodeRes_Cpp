36a696bb573730e4c203a8aee19e4605c2762103
sunchao14 <sunchao14@meituan.com>
2017-11-18 01:01:40 +0800

    saved

---
+++ boost/crc.cpp
@@ -0,0 +1,42 @@
+#include <boost/crc.hpp>
+#include <boost/format.hpp>
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+uint32_t GetCrc32(const string& my_string) 
+{
+    boost::crc_32_type result;
+    result.process_bytes(my_string.data(), my_string.length());
+    return result.checksum();
+}
+
+
+uint64_t get_id(int iType, int iDetailType, const std::string &strPhrase)
+{
+    uint64_t ret = 0;
+    ret += (uint64_t)iType << 48;
+    ret += (uint64_t)iDetailType << 32;
+
+    boost::crc_32_type crcChecker;
+    crcChecker.process_bytes(strPhrase.data(), strPhrase.length());
+    ret += crcChecker.checksum();
+
+    return ret;
+}
+
+int main()
+{
+    // string str;
+    // while (getline(cin, str)) {
+        // cout << boost::format("%08x") % GetCrc32(str) << endl;
+    // } // while
+    
+    cout << boost::format("%08x") % GetCrc32("烤鸭") << endl;
+    cout << boost::format("%016x") % get_id(1, 3, "烤鸭") << endl;
+
+    return 0;
+}
+
+
