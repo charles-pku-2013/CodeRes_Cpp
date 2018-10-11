843ced360a31f052e714295c9dc9ded66b16b15b
charles-pku-2013 <chao.sun.2010@gmail.com>
2018-10-11 22:18:07 +0800

    saved

--- 2DArray.cpp
+++ 2DArray.cpp
@@ -2,8 +2,8 @@
 
 void func( int (*a)[3] )
 {
-    
-    
+
+
 }
 
 int main()
@@ -13,15 +13,15 @@ int main()
                     {4,5,6},
                     {7,8,9}
                 };
-    
+
 //    int b[3] = {10,20,30};
-    
+
 //    func(a);
-    
+
     printf( "%x\n", a );
     printf( "%x\n", a+1 );
     printf( "%x\n", a[1] );
     printf( "%x\n", a[1]+1 );
-    
+
     return 0;
 }
