268bd29cd9dc49cf8ff413bb11098aa22130944e
sunchao62 <sunchao11@jd.com>
2020-01-17 13:37:01 +0800

    save

--- fun_ptr_callable_overload.cpp
+++ fun_ptr_callable_overload.cpp
@@ -1,34 +1,56 @@
 #include <iostream>
 #include <vector>
+#include <unordered_map>
 #include <functional>
 
 using namespace std;
 
-int test_fun(const std::string &str, std::vector<float> *result) {
-    cout << "test_fun" << endl;
+int float_fun(const std::string &str, std::vector<float> *result) {
+    cout << "float_fun" << endl;
     return 0;
 }
 
-struct Foo {
+int string_fun(const std::string &str, std::vector<std::string> *result) {
+    cout << "string_fun" << endl;
+    return 0;
+}
+
+struct Callable {
+    Callable() = default;
+    explicit Callable(void *p) : pfn_(p) {}
+
     int operator()(const std::string &str, std::vector<std::string> *result) {
-        return 0;
+        using FunPtr = int(*)(const std::string&, std::vector<std::string>*);
+        FunPtr pfn = (FunPtr)pfn_;
+        return pfn(str, result);
     }
     int operator()(const std::string &str, std::vector<float> *result) {
         // typedef int(*FunPtr)(const std::string&, std::vector<float>*);
         using FunPtr = int(*)(const std::string&, std::vector<float>*);
-        FunPtr pfn = &test_fun;
+        FunPtr pfn = (FunPtr)pfn_;
         return pfn(str, result);
     }
 
-    void *func = nullptr;
+    void *pfn_ = nullptr;
 };
 
 int main() {
+    std::unordered_map<std::string, Callable> table;
+    table["string"] = Callable((void*)&string_fun);
+    table["float"] = Callable((void*)&float_fun);
 
-    string s("abc def");
-    Foo foo;
-    std::vector<float> arr;
-    foo(s, &arr);
+    do {
+        std::string s("abc");
+        std::vector<std::string> arr;
+        auto it = table.find("string");
+        (it->second)(s, &arr);
+    } while (0);
+    do {
+        std::string s("abc");
+        std::vector<float> arr;
+        auto it = table.find("float");
+        (it->second)(s, &arr);
+    } while (0);
 
     return 0;
 }
