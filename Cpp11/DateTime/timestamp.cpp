5a1bb22eeeae31d618893df4deea2aeb97b6d460
sunchao62 <sunchao11@jd.com>
2020-07-15 01:30:23 +0800

    save

--- Cpp11/DateTime/timestamp.cpp
+++ Cpp11/DateTime/timestamp.cpp
@@ -56,3 +56,48 @@ int main() {
     return 0;
 }
 
+namespace example1 {
+bool TFSPredictor::_SaveErrReq(const Request &req, const std::string &err_msg) {
+    if (!save_err_req_) { return false; }
+
+    try {
+        char buf[64] = {'\0'};
+        struct timeval tv;
+        gettimeofday(&tv, NULL);
+        std::tm tm;
+        localtime_r(&tv.tv_sec, &tm);
+        std::size_t n = std::strftime(buf, sizeof(buf), "%Y%m%d%H%M%S", &tm);
+        snprintf(buf + n, sizeof(buf) - n, "_%06ld", tv.tv_usec);
+
+        // save binary req data
+        if (!save_req(&req, err_req_dir_ + "/" + buf + ".dat")) {
+            LOG(ERROR) << DebugString() << " save request fail!";
+            return false;
+        }
+
+        // save err msg
+        std::ofstream ofs(err_req_dir_ + "/" + buf + ".txt");
+        if (!ofs) {
+            LOG(ERROR) << DebugString() << " save error msg fail!";
+            return false;
+        }
+        ofs << err_msg << std::endl;
+
+        return true;
+    } catch (const std::exception &ex) {
+        LOG(ERROR) << DebugString() << " _SaveErrReq error: " << ex.what();
+        return false;
+    } catch (...) {
+        LOG(ERROR) << DebugString() << " _SaveErrReq unknown error!";
+        return false;
+    }
+}
+
+template<typename ProtoType>
+inline bool save_req(const ProtoType *msg, const std::string &fname) {
+    std::ofstream ofs(fname, std::ios::out | std::ios::binary);
+    if (!ofs) { return false; }
+    return msg->SerializeToOstream(&ofs);
+}
+}  // namespace example1
+
