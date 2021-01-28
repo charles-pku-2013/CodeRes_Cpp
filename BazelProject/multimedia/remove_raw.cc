00724102b2a3f770d19b59634f4988a52e6f34f1
sunchao62 <sunchao11@jd.com>
2021-01-28 11:43:26 +0800

    save

--- BazelProject/multimedia/remove_raw.cc
+++ BazelProject/multimedia/remove_raw.cc
@@ -3,9 +3,7 @@
 #include <set>
 #include <iostream>
 #include <boost/filesystem.hpp>
-#include "absl/strings/str_cat.h"
 #include "absl/strings/str_join.h"
-#include "absl/strings/str_format.h"
 #include "absl/strings/strip.h"
 #include "absl/strings/match.h"
 #include <glog/logging.h>
@@ -17,6 +15,18 @@ DEFINE_bool(y, false, "remove without confirm");
 
 
 namespace fs = boost::filesystem;
+
+template<typename Iter>
+void remove_files(Iter beg, Iter end) {
+    for(; beg != end; ++beg) {
+        try {
+            LOG(INFO) << "Removing " << *beg;
+            fs::remove(*beg);
+        } catch (const std::exception &ex) {
+            LOG(ERROR) << "Fail to remove file " << *beg << ". " << ex.what();
+        }
+    }
+}
 
 int main(int argc, char **argv)
 try {
@@ -32,16 +42,40 @@ try {
     fs::current_path(FLAGS_dir);
 
     std::set<std::string> to_be_removed;
-    for (fs::directory_iterator itr(path); itr != fs::directory_iterator(); ++itr) {
+    for (fs::directory_iterator itr("."); itr != fs::directory_iterator(); ++itr) {
         if (!fs::is_regular_file(*itr)) { continue; }
-        if (absl::EqualsIgnoreCase(itr->path().extension(), FLAGS_raw)) {
+        if (absl::EqualsIgnoreCase(itr->path().extension().string(), FLAGS_raw)) {
             std::string jpg_file1 = itr->path().stem().string() + ".jpg";
             std::string jpg_file2 = itr->path().stem().string() + ".JPG";
             if (!fs::exists(jpg_file1) && !fs::exists(jpg_file2)) {
-                to_be_removed.insert(itr->path().string());
+                to_be_removed.insert(itr->path().filename().string());
             }
         }
     }  // for itr
+
+    if (to_be_removed.empty()) {
+        LOG(INFO) << "No file found";
+        return 0;
+    }
+
+    if (FLAGS_y) {
+        remove_files(to_be_removed.begin(), to_be_removed.end());
+    } else {
+        std::cerr << "The following files will be removed:\n"
+            << absl::StrJoin(to_be_removed, "\n")
+            << "Please confirm (Y/n): " << std::flush;
+        std::string input;
+        std::getline(std::cin, input);
+        absl::StripAsciiWhitespace(&input);
+        if (absl::EqualsIgnoreCase(input, "y")) {
+            remove_files(to_be_removed.begin(), to_be_removed.end());
+        } else {
+            LOG(INFO) << "Aborted!";
+            return 0;
+        }
+    }
+
+    return 0;
 
 } catch (const std::exception &ex) {
     std::cerr << argv[0] << " terminated by exception: " << ex.what() << std::endl;
