7bcc2ba4844b1022becddd3c40cd27bd51c79d05
sunchao62 <sunchao11@jd.com>
2022-03-25 18:32:39 +0800

    save

--- protobuf_rpc/save_load_parse.cpp
+++ protobuf_rpc/save_load_parse.cpp
@@ -50,3 +50,28 @@ bool ClassName::_SaveErrReq(const RequestNew &req, const std::string &err_msg) {
         return false;
     }
 }
+
+#if 0
+文本互转 parse protobuf
+# from DebugString()
+#include "google/protobuf/message.h"
+#include "google/protobuf/text_format.h"
+if (!google::protobuf::TextFormat::ParseFromString(content, &cfg)) {
+    LOG(ERROR) << "Fail to parsxe model config file " << config_file;
+    return false;
+}
+
+# to json
+#include "google/protobuf/util/json_util.h"
+using protobuf::util::MessageToJsonString;
+JsonPrintOptions opts;
+opts.add_whitespace = true;
+opts.always_print_primitive_fields = true;
+const auto& status = MessageToJsonString(response, output, opts);
+if (!status.ok()) {...}
+
+# from json
+LIBPROTOBUF_EXPORT util::Status JsonStringToMessage(const string& input,
+                                   Message* message,
+                                   const JsonParseOptions& options);
+#endif
