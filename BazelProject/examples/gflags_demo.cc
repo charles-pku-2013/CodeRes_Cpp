e5a8f299cbc85e801bf9ee981fea0e6ff7eb0560
sunchao62 <sunchao11@jd.com>
2023-11-13 16:47:12 +0800

    save

--- BazelProject/examples/gflags_demo.cc
+++ BazelProject/examples/gflags_demo.cc
@@ -83,6 +83,7 @@ void test1( int argc, char **argv )
 
 int main( int argc, char **argv )
 {
+    gflags::AllowCommandLineReparsing();
     int idx = gflags::ParseCommandLineFlags(&argc, &argv, true);
 
     // 未被gflags识别的参数
--- google-glog/demo.cpp
+++ google-glog/demo.cpp
@@ -9,19 +9,39 @@
  * stderrthreshold
  * log_dir
  *
- * INFO, WARNING, ERROR, and FATAL. 
- * Logging a FATAL message terminates the program (after the message is logged). 
+ * INFO, WARNING, ERROR, and FATAL.
+ * Logging a FATAL message terminates the program (after the message is logged).
  */
 
+namespace {
+/*
+ *    glog settings:
+ *    env                   arg
+ *    GLOG_max_log_size   --max_log_size (MB)
+ *    GLOG_log_dir        --log_dir
+ *    GLOG_logtostderr    --logtostderr (全部输出到stderr不会生成log文件)
+ */
+void init_log() {
+    google::InitGoogleLogging(FLAGS_server_name.c_str());
+    google::SetLogDestination(google::INFO, (FLAGS_server_log_path + "info_").c_str());
+    google::SetLogDestination(google::WARNING, (FLAGS_server_log_path + "warning_").c_str());
+    google::SetLogDestination(google::ERROR, (FLAGS_server_log_path + "error_").c_str());
+    if (FLAGS_debug) {
+        google::SetStderrLogging(google::INFO);  // 同时输出到stderr和log文件
+    } else {
+        google::SetStderrLogging(google::ERROR);
+    }
+}
+}  // namespace
 
 int main(int argc, char* argv[])
 {
-	// Initialize Google's logging library.
-	google::InitGoogleLogging(argv[0]);
+    // Initialize Google's logging library.
+    // google::InitGoogleLogging(argv[0]);
 
-	// ...
+    // ...
     int num_cookies = 25;
-	LOG(INFO) << "Found " << num_cookies << " cookies";
+    LOG(INFO) << "Found " << num_cookies << " cookies";
 
     return 0;
 }
