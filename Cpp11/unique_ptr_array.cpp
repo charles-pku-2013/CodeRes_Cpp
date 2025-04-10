758807c1ae71a4e58ed253550ab3b6318e724afd
charles <289185927@qq.com>
2025-04-10 09:06:35 +0800

    local save

--- Cpp11/unique_ptr_array.cpp
+++ Cpp11/unique_ptr_array.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <string>
 #include <memory>
+#include <iterator>
+
+using namespace std;
 
 struct Foo {
     Foo() {
@@ -16,11 +19,20 @@ struct Foo {
     }
 
     int data_ = 0;
+
+    friend std::ostream& operator<<(std::ostream& os, const Foo& foo) {
+        os << foo.data_;
+        return os;
+    }
 };
 
 
 int main() {
-    std::unique_ptr<Foo[]> ptr(new Foo[10]{[0 ... 9] = {5}});
+    // std::unique_ptr<Foo[]> ptr(new Foo[10]{[0 ... 9] = {5}});
+    std::unique_ptr<Foo[]> ptr(new Foo[10]{1,2,3,4,5,0});   // only init first elem
+
+    std::copy(ptr.get(), ptr.get() + 10, std::ostream_iterator<Foo>(cout, " "));
+    cout << endl;
 
     return 0;
 }
